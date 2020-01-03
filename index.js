b934676c3d6a8f97e92bb7bbd5118c75246bbaaf
lijianru <2219269374@qq.com>
2020-01-03 20:59:26 +0800

    create index

--- index.js
+++
@@ -1,3 +0,0 @@
-const html = require('./src/demo/index.md');
-const root = document.getElementById('root');
-root.innerHTML = html;
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,6 @@
 const path = require('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 
-const fs = require('fs')
-
-const filePath = path.resolve(__dirname, './src')
-
-fileDisplay(filePath)
-
-function fileDisplay(filePath) {
-  fs.readdir(filePath, function (error, files) {
-    if (error) {
-      console.error(error)
-    } else {
-      files.forEach(function (filename) {
-        const fileDir = path.join(filePath, filename)
-        fs.stat(fileDir, function (error, stats) {
-          if (error) {
-            console.error(error)
-          } else {
-            const isFile = stats.isFile()
-            const isDir = stats.isDirectory()
-            if (isFile) {
-              console.log('path: ', fileDir)
-            }
-            if (isDir) {
-              fileDisplay(fileDir)
-            }
-          }
-        })
-      })
-    }
-  })
-}
-
 module.exports = {
   entry: {
     main: path.join(__dirname, './index.js')
