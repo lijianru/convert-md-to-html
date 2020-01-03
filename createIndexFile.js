34b9d37f61d72746256d439e9d4f07744cccca55
lijianru <2219269374@qq.com>
2020-01-03 20:56:41 +0800

    create index

--- createIndexFile.js
+++ createIndexFile.js
@@ -6,6 +6,11 @@ const filePath = path.resolve(__dirname, './src')
 fileDisplay(filePath)
 
 function fileDisplay(filePath) {
+  let content =
+`
+const root = document.getElementById('root');
+root.innerHTML = html;
+`
   fs.readdir(filePath, function (error, files) {
     if (error) {
       console.error(error)
@@ -19,7 +24,8 @@ function fileDisplay(filePath) {
             const isFile = stats.isFile()
             const isDir = stats.isDirectory()
             if (isFile) {
-              console.log('./src' + fileDir.replace(/\\/g, '/').split('src')[1])
+              const requireFile = fileDir.replace(/\\/g, '/').split('src')[1]
+              writeFile(`const html = require('./src${requireFile}');` + content)
             }
             if (isDir) {
               fileDisplay(fileDir)
@@ -28,5 +34,15 @@ function fileDisplay(filePath) {
         })
       })
     }
+  })
+}
+
+function writeFile(content) {
+  console.log('content', content)
+  fs.writeFile('index.js', content, 'utf8', (error) => {
+    if (error) {
+      console.error(error)
+    }
+    console.log('文件已经创建成功！！！！！！')
   })
 }
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const html = require('./src/demo/index.md')
-const root = document.getElementById('root')
-
-root.innerHTML = html
+const html = require('./src/demo/index.md');
+const root = document.getElementById('root');
+root.innerHTML = html;
