28fe9e1fb0ab71444bbc24c0e77ad6a1254947fa
lijianru <2219269374@qq.com>
2020-01-17 15:32:01 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-const indexedDB使用md1578997579879 = require('./src/JavaScript/indexedDB/indexedDB使用.md');
-const 灵活使用consolemd1578997579879 = require('./src/JavaScript/灵活使用console.md');
-const markdown语法md1578997579879 = require('./src/markdown语法.md');
-const 浏览器缓存md1578997579879 = require('./src/浏览器/浏览器缓存.md');
-import style from "./theme.css";
+import * as readme from './README.md'
+import "./theme.css";
 
 const root = document.getElementById('root');
 
-root.innerHTML = `<h1 id="indexedDB使用.md">indexedDB使用.md</h1><div>${indexedDB使用md1578997579879}</div><hr />
-<h1 id="灵活使用console.md">灵活使用console.md</h1><div>${灵活使用consolemd1578997579879}</div><hr />
-<h1 id="markdown语法.md">markdown语法.md</h1><div>${markdown语法md1578997579879}</div><hr />
-<h1 id="浏览器缓存.md">浏览器缓存.md</h1><div>${浏览器缓存md1578997579879}</div><hr />`
+root.innerHTML = `<h1 id="readme">Readme.md</h1><div>${readme}</div>`
